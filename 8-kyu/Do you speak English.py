8c28496e7959adaa481565f3440f33e99bda88ae
Forhad <md.foredul.islam.forhad@gmail.com>
2026-08-06 14:14:04 +0600

    Do you speak English problem solved

---
+++ 8-kyu/Do you speak English.py
@@ -0,0 +1,15 @@
+# Solution: 01
+
+def sp_eng(sentence):
+    if 'english' in sentence.lower():
+        return True
+    else:
+        return False
+
+
+# Enhanced Solution
+
+
+def sp_eng2(sentence):
+   return 'english' in sentence.lower()
+
